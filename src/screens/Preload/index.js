0a91802d1e2f2b789dfa9c87809ef33597497d27
mac <neycariom@gmail.com>
2023-11-15 02:30:30 +0100

    preload terminado

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,19 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import { Container, LoginIcon } from './styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { userContext } from '../../contexts/UserContext';
+
+import Api from '../../Api';
 
 import BarberLogo from '../../assets/barber.svg';
 
 export default function Preload() {
-
+    const { dispatch: userDispatch } = useContext(userContext);
     const navigation = useNavigation();
 
     useEffect(() => {
         const checkToken = async () => {
             const token = await AsyncStorage.getItem('token');
+
             if (token) {
-                //validar o token
+                let res = await Api.checkToken(token);
+
+                if (res.token) {
+                    await AsyncStorage.setItem('token', res.token);
+
+                    userDispatch({
+                        type: 'setAvatar',
+                        payload: {
+                            avatar: res.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes: [{ name: 'MainTab' }]
+                    });
+                } else {
+                    navigation.navigate('SignIn');
+                }
             } else {
                 navigation.navigate('SignIn');
             }
